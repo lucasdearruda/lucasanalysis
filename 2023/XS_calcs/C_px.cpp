1249c1f2a97ef0f8145c8ebe105c38ff589fce89
lucasdearruda <lucas.dearruda@physics.uu.se>
2025-02-05 14:00:37 +0100

    kind of working

---
+++ 2023/XS_calcs/C_px.cpp
@@ -0,0 +1,174 @@
+//
+// Script for evaluation the cross section for protons in Carbon__ 2023 runs
+//
+#include "TF1.h"
+#include "TH1D.h"
+#include "TVirtualFitter.h"
+#include "TPaletteAxis.h"
+#include "TMath.h"
+#include "TLatex.h"
+#include "TChain.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "/mnt/medley/LucasAnalysis/useful.h" //version6.10.2024.0002
+
+#include "TStopwatch.h"
+#include <time.h>
+
+Double_t mc = 0.0408;//g 
+Double_t M = 12; //g/mol
+Double_t NA = 6.02214076e23; //mol^-1
+Double_t Nc = (mc/M) * NA; //atoms/cm^3
+Double_t omega_tel = 0.040 ;//sr 
+
+
+Int_t Ta;
+
+void C_px(Double_t Ea = 26.0, Double_t Eb = 27.0 ,Int_t runa = 35,Int_t runb = 39){
+TStopwatch timer;
+
+
+TChain *tx = new TChain("M");
+
+
+string processed_runs = "/mnt/medley/LucasAnalysis/2023/reducedv61";
+
+string name;
+for(Int_t i=runa;i<=runb;i++)
+{
+    Bool_t fExist = true;
+    name = Form("%s/%03d.root",processed_runs.c_str(),i);
+    cout << name << endl;
+    ifstream mfile;
+    mfile.open(name);
+    if(mfile)
+    {
+        mfile.close();
+    }
+    else
+        fExist=false;
+    if(fExist)
+    {	  
+        cout << "Adding " << name << endl;
+        tx->Add(name.c_str());
+        Ta = (Int_t) (tx->GetEntries());
+        cout << "Entries " << Ta/1000 << "k "  << endl;
+    }
+    
+}
+
+cout<<"---------------------------------------------------"<<endl;
+TTree *InfoTree = new TTree("InfoTree", "InfoTree");
+InfoTree->ReadFile("/mnt/medley/LucasAnalysis/2023/runlist.csv", "RunN/I:Or/C:Target/C:Time_s/I:Time_h/F:TimeEval_s/I:TimeEval_h/F:ChargeIntegrator/F:ChargeFaraday/F");
+
+Int_t RunN;
+Float_t ChargeFaraday;
+Float_t TotalChargeFaraday = 0;
+
+InfoTree->SetBranchAddress("RunN", &RunN);
+InfoTree->SetBranchAddress("ChargeFaraday", &ChargeFaraday);
+
+for(int i=0;i<InfoTree->GetEntries();i++)
+{
+    InfoTree->GetEntry(i);
+    //cout<< i<<", nrun = "<<RunN<< endl;
+    if (RunN<= runb && RunN>=runa)
+    {
+        //InfoTree->GetEntry(i);
+        cout << "RunN: " << RunN << " ChargeFaraday: " << ChargeFaraday << endl;
+        TotalChargeFaraday += ChargeFaraday;
+    }
+}
+cout<<"---------------------------------------------------"<<endl;
+cout<< "Total Charge Faraday: " << TotalChargeFaraday <<" µC"<< endl;
+
+
+TFile *ff = new TFile("/mnt/medley/LucasAnalysis/2023/nflux_direct/nflux.root", "READ");
+TH1D *nflux = (TH1D*)ff->Get("nflux");
+TCanvas * cflux = new TCanvas("cflux","cflux");
+nflux->Draw();
+
+Double_t nflux_En = nflux->GetBinContent(nflux->FindBin((Ea+Eb)/2.0));
+cout << "nflux_En: " << nflux_En << " n / sr / µC / 1-MeV."<<endl;// --> "<<nflux_En*(Eb-Ea)<< " n / sr / µC ."<<endl;
+
+
+TCanvas * hp_cv = new TCanvas("hp","hp");
+TH1D *hp = new TH1D("hp","hp",100,0,40);
+tx->Draw("E>>hp",Form("ENN>%f && ENN<%f && PID==1 && ang == 20",Ea, Eb));
+TCanvas * xs_cv = new TCanvas("XS","XS");
+
+Double_t L = 464.72; //cm
+Double_t cm2_to_barn = 1e+24 ;//cm²
+Double_t Factor = L*L/(Nc*omega_tel*TotalChargeFaraday*nflux_En);
+Factor = Factor*cm2_to_barn;
+cout<<"Factor: "<<Factor<<endl;
+
+TH1D *xsp = new TH1D("xsp","xsp",100,0,40);
+for(int i=1;i<=hp->GetNbinsX();i++)
+{
+    xsp->SetBinContent(i,1e3*Factor*hp->GetBinContent(i));//1e3 changes to mb
+}
+xsp->GetYaxis()->SetTitle("mb/sr#dot1-MeV");
+xsp->GetXaxis()->SetTitle("E_{protons} (MeV)");
+//xsp->Draw();
+
+TTree *txs = new TTree("txs","txs");
+
+//E          ANG        DATA      1ERR-T     1DATA      2ERR-T     2
+//MEV        ADEG       MB/SR/MEV  MB/SR/MEV  MB/SR/MEV  MB/SR/MEV
+txs->ReadFile("/mnt/medley/LucasAnalysis/exfor/exfor_files/n.xP_26.5MeV_Slypen.csv","E/F:Ang/F:XS/F:XSerr/F");
+//new TCanvas();
+
+float E, Ang, XS, XSerr;
+
+// 3. Use SetBranchAddress para associar as variáveis aos ramos da árvore
+txs->SetBranchAddress("E", &E);
+txs->SetBranchAddress("Ang", &Ang);
+txs->SetBranchAddress("XS", &XS);
+txs->SetBranchAddress("XSerr", &XSerr);
+
+// 4. Crie o TGraphErrors para armazenar os pontos e os erros
+TGraphErrors *graph = new TGraphErrors();
+
+// 5. Loop na árvore com a condição 'Ang == 20'
+int nEntries = txs->GetEntries();
+for (int i = 0; i < nEntries; i++) {
+    // Carregue os dados para a entrada i
+    txs->GetEntry(i);
+
+    // Verifique a condição (Ang == 20)
+    if (Ang == 20) {
+        // Adicione o ponto ao gráfico, incluindo o erro
+        graph->SetPoint(graph->GetN(), E, XS);             // Adiciona o ponto
+        graph->SetPointError(graph->GetN() - 1, 0, XSerr); // Adiciona o erro no X (XSerr) e no Y (0, pois não temos erro em Y)
+    }
+}
+graph->SetMarkerStyle(20);  // 20 é o código para círculos
+graph->SetMarkerSize(1.5);  // Tamanho dos pontos (ajuste conforme necessário)
+graph->SetMarkerColor(kRed); 
+graph->SetLineColor(kRed); 
+// 6. Desenhe o gráfico
+graph->GetYaxis()->SetTitle("mb/sr#dot1-MeV");
+graph->GetXaxis()->SetTitle("E_{protons} (MeV)");
+graph->Draw("ALP");  // "A" para eixos, "P" para pontos
+
+graph->SetTitle("C(n,Xp), 20^{o}, 2023");
+
+xsp->SetLineWidth(2);
+xsp->SetFillColor(kBlue);
+xsp->SetFillStyle(3004);
+xsp->Draw("same");
+
+
+gPad->SetGridx();
+gPad->SetGridy();
+
+TLegend *tl = new TLegend(0.54,0.63,0.85,0.85);
+tl->AddEntry(xsp,"Slypen+(200)","lpf");
+tl->AddEntry(graph,"Medley (2023)","lpf");
+tl->Draw();
+
+timer.Print();
+}
+
