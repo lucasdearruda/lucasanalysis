746d303da8b35b6f340f936e8e720746e84fba4a
lucasdearruda <lucas.dearruda@physics.uu.se>
2025-05-26 12:37:45 +0200

    with better names

--- 2023/XS_calcs/produce_pXS.cpp
+++ 2023/XS_calcs/produce_pXS.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include "TStopwatch.h"
 
+
+//define binwidth in a HH file 
 void produce_pXS(Float_t Ea = 25, Float_t Eb = 26, Float_t binMeV = 1, string filename = "Fe_pXS.root"){ 
     
     TStopwatch timer;
@@ -17,6 +19,7 @@ void produce_pXS(Float_t Ea = 25, Float_t Eb = 26, Float_t binMeV = 1, string fi
     Float_t intg;
 
     Int_t nEbins = 0;
+    //Add some warning for safety 
     for (Float_t i = Ea; i < Eb; i+=binMeV) {
         nEbins++;
     }//calculate the number of histograms
@@ -38,6 +41,8 @@ void produce_pXS(Float_t Ea = 25, Float_t Eb = 26, Float_t binMeV = 1, string fi
 
 
     Float_t cos_angle, angle, En;
+    Float_t Ea_zero, Eb_zero;
+    Int_t intEa_zero, intEb_zero,decEa_zero, decEb_zero,intEn, decEn;
 
     TCanvas *Cv[nEbins];
     for(int i = 0; i < nEbins; i++) {
@@ -47,47 +52,63 @@ void produce_pXS(Float_t Ea = 25, Float_t Eb = 26, Float_t binMeV = 1, string fi
         Cv[i]->Divide(3,3);
     }
 
-    
+    string target_details = "Fe_thick";
     // for each energy bin, create histograms and fill the graphs
-    Float_t Ea_zero, Eb_zero;
+    
     for (int i = 0; i < nEbins; i++) {
         //calculating the energy:
         En = Ea + (i+0.5)*binMeV;
         Ea_zero = Ea + i * binMeV;
         Eb_zero = Ea + (i+1) * binMeV;
+
+        //Convert parts to mode X.Y --> XpY, so it gets better to load up the names of the objects
+        intEn = static_cast<int>(En); // Convert to integer for binning
+        decEn = static_cast<int>(En * 10) % 10;
+
+        intEa_zero = static_cast<int>(Ea_zero); // Convert to integer for binning
+        decEa_zero = static_cast<int>(Ea_zero * 10) % 10;
+        
+        intEb_zero = static_cast<int>(Eb_zero); // Convert to integer for binning
+        decEb_zero = static_cast<int>(Eb_zero * 10) % 10;
+        
+        
+        std::cout<<"########################## debugging info ##########################"<<std::endl;
+        std::cout<< "Processing energy bin: " << i << " with En: " << En << ", Ea_zero: " << Ea_zero << ", Eb_zero: " << Eb_zero << std::endl;
+        std::cout << "intEn: " << intEn << " decEn: " << decEn << " intEa_zero: " << intEa_zero << " decEa_zero: " << decEa_zero << " intEb_zero: " << intEb_zero << " decEb_zero: " << decEb_zero << std::endl;
+        std::cout<<" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "<<std::endl;
         std::cout << "Energy bin: " << i << " En: " << En << " Ea: " << Ea_zero << " Eb: " << Eb_zero << std::endl;
         //receive string of histograms;
         h = Fe_allAngles2(Ea+i*binMeV, Ea+(i+1)*binMeV);
         hist[0][i] = h[0]; //20deg
-        hist[0][i]->SetNameTitle(Form("h_20deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 20 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[0][i]->SetNameTitle(Form("%s_20deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 20 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[0][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[0][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
         hist[1][i] = h[1]; //40deg
-        hist[1][i]->SetNameTitle(Form("h_40deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 40 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[1][i]->SetNameTitle(Form("%s_40deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 40 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[1][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[1][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
         hist[2][i] = h[2]; //60deg
-        hist[2][i]->SetNameTitle(Form("h_60deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 60 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[2][i]->SetNameTitle(Form("%s_60deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 60 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[2][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[2][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
         hist[3][i] = h[3]; //80deg
-        hist[3][i]->SetNameTitle(Form("h_80deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 80 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[3][i]->SetNameTitle(Form("%s_80deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 80 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[3][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[3][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
         hist[4][i] = h[4];  //100deg
-        hist[4][i]->SetNameTitle(Form("h_100deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 100 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[4][i]->SetNameTitle(Form("%s_100deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 100 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[4][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[4][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
         hist[5][i] = h[5];  //120deg
-        hist[5][i]->SetNameTitle(Form("h_120deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 120 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[5][i]->SetNameTitle(Form("%s_120deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 120 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[5][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[5][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
         hist[6][i] = h[6];  //140deg
-        hist[6][i]->SetNameTitle(Form("h_140deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 140 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[6][i]->SetNameTitle(Form("%s_140deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 140 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[6][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[6][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
         hist[7][i] = h[7];  //160deg
-        hist[7][i]->SetNameTitle(Form("h_160deg_En_%.1f_%.1f_MeV_bin%d",Ea_zero,Eb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 160 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
+        hist[7][i]->SetNameTitle(Form("%s_160deg_En_%dp%d_%dp%d_MeV_bin%d",target_details.c_str(),intEa_zero,decEa_zero,intEb_zero,decEb_zero,i),Form("Fe(n,Xp) #theta_{LAB} = 160 deg, E_{NN} = %.1f to %.1f MeV",Ea_zero, Eb_zero));
         hist[7][i]->GetXaxis()->SetTitle("E_p (MeV)");
         hist[7][i]->GetYaxis()->SetTitle("d^{2}#sigma/d#OmegadE (mb/sr/MeV)");
 
@@ -109,7 +130,7 @@ void produce_pXS(Float_t Ea = 25, Float_t Eb = 26, Float_t binMeV = 1, string fi
 
             intg = h[j]->Integral("width");
             
-            XSgr->AddPoint(angle,En,intg);
+            XSgr->AddPoint(En,angle,intg);
             
             gAngle[i]->SetPoint(j, angle,intg);
             gCosAngle[i]->SetPoint(j, cos_angle,intg);
@@ -118,12 +139,12 @@ void produce_pXS(Float_t Ea = 25, Float_t Eb = 26, Float_t binMeV = 1, string fi
         }
 
         gAngle[i]->SetTitle(Form("Fe(n,Xp) - d#sigma/d#Omega (mb/sr) for %02.1f MeV",En));
-        gAngle[i]->SetName(Form("ddOmega_%02.1f_MeV",En));
+        gAngle[i]->SetName(Form("ddOmega_%s_%dp%d_MeV",target_details.c_str(),intEn,decEn));
         gAngle[i]->GetXaxis()->SetTitle("Angle (deg)");
         gAngle[i]->GetYaxis()->SetTitle("d#sigma/d#Omega (mb/sr)");
         //gAngle[i]->Write();
         gCosAngle[i]->SetTitle(Form("Fe(n,Xp) - d#sigma/d#Omega (mb/sr) for %02.1f MeV",En));
-        gCosAngle[i]->SetName(Form("ddOmega_%02.1f_MeV_cos",En));
+        gCosAngle[i]->SetName(Form("ddOmega_%s_%dp%d_MeV_cos",target_details.c_str(),intEn,decEn));
         gCosAngle[i]->GetXaxis()->SetTitle("cos(#theta)");
         gCosAngle[i]->GetYaxis()->SetTitle("d#sigma/d#Omega (mb/sr)");
         //gCosAngle[i]->Write();
@@ -143,6 +164,11 @@ void produce_pXS(Float_t Ea = 25, Float_t Eb = 26, Float_t binMeV = 1, string fi
     }
 
     ff->cd();
+    XSgr->SetName(Form("%s_DDX", target_details.c_str()));
+    XSgr->SetTitle("Fe(n,Xp) - d^{2}#sigma/d#OmegadE (mb/sr/MeV) vs Angle and Energy");
+    XSgr->GetYaxis()->SetTitle("Angle (deg)");
+    XSgr->GetXaxis()->SetTitle("Energy (MeV)");
+    XSgr->GetZaxis()->SetTitle("d#sigma/d#Omega (mb/sr)");
     XSgr->Write();
     TCanvas *cXS = new TCanvas("cXS","cXS");
     XSgr->Draw();
