692049bf6acfeb58e3eb652f98df3a16b92c7275
lucasdearruda <lucas.dearruda@physics.uu.se>
2025-08-27 13:39:32 +0200

    cuts for off events

---
+++ 2023/PIDv6/E_ENN/p_offevts2.C
@@ -0,0 +1,18 @@
+{
+//========= Macro generated from object: offevts1/Graph
+//========= by ROOT version6.32.08
+   
+   TCutG *cutg = new TCutG("offevts2",6);
+   cutg->SetVarX("ENN");
+   cutg->SetVarY("E");
+   cutg->SetTitle("Graph");
+   cutg->SetFillStyle(1000);
+   cutg->SetLineWidth(2);
+   cutg->SetPoint(0,0.542908,0.730399);
+   cutg->SetPoint(1,1.74937,2.48706);
+   cutg->SetPoint(2,43.3986,47.5129);
+   cutg->SetPoint(3,0.0183588,47.5129);
+   cutg->SetPoint(4,-0.0340961,0.961538);
+   cutg->SetPoint(5,0.542908,0.730399);
+//   cutg->Draw("");
+}
